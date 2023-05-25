7589ed545a7ec7c50ebf324cedc464e3827a1fbf
Rob Beal <rob.beal@yotoplay.com>
2023-05-25 20:38:06 +0100

    chore: initial commit

---
+++ index.js
@@ -0,0 +1,31 @@
+module.exports = {
+  env: {
+    node: true,
+    commonjs: true,
+    jest: true,
+    es6: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "prettier",
+  ],
+  parserOptions: {
+    sourceType: "module",
+    ecmaVersion: 2022,
+  },
+  plugins: ["unicorn"],
+  rules: {
+    "space-infix-ops": ["error"],
+    eqeqeq: ["error", "always"],
+    "prefer-template": ["error"],
+    "@typescript-eslint/no-explicit-any": "off",
+    "unicorn/prefer-module": "off",
+    "unicorn/filename-case": [
+      "error",
+      {
+        case: "camelCase",
+      },
+    ],
+  },
+};
