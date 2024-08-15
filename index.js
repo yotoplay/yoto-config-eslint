4e71a0b6afcb9f25c0e7b3d7fae88b5f52f04fbb
Rob Beal <rob.beal@yotoplay.com>
2024-08-15 17:02:10 +0100

    feat: upgrade to eslint 9

--- index.js
+++ index.js
@@ -1,34 +1,42 @@
-module.exports = {
-  env: {
-    node: true,
-    commonjs: true,
-    jest: true,
-    es6: true,
+import typescriptEslint from "@typescript-eslint/eslint-plugin";
+import typescriptParser from "@typescript-eslint/parser";
+import prettier from "eslint-config-prettier";
+import unicorn from "eslint-plugin-unicorn";
+
+export default [
+  {
+    ignores: ["node_modules/*", "*.json", "coverage/*", "dist/*"],
   },
-  extends: [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
-    "prettier",
-  ],
-  parserOptions: {
-    sourceType: "module",
-    ecmaVersion: 2022,
-  },
-  plugins: ["unicorn"],
-  rules: {
-    "no-console": [ "error" ],
-    "space-infix-ops": ["error"],
-    eqeqeq: ["error", "always"],
-    "prefer-template": ["error"],
-    "@typescript-eslint/no-explicit-any": "off",
-    "unicorn/prefer-module": "off",
-    "unicorn/filename-case": [
-      "error",
-      {
-        case: "camelCase",
-        ignore: ["jest-mongodb-config.js"],
+  {
+    files: ["**/*.ts", "**/*.tsx"],
+    languageOptions: {
+      parser: typescriptParser,
+      parserOptions: {
+        ecmaVersion: 2023,
+        sourceType: "module",
       },
-    ],
+    },
+    plugins: {
+      "@typescript-eslint": typescriptEslint,
+      unicorn,
+    },
+    rules: {
+      "@typescript-eslint/no-explicit-any": "off",
+      "unicorn/prefer-module": "off",
+      "unicorn/filename-case": [
+        "error",
+        {
+          case: "camelCase",
+          ignore: ["jest-mongodb-config.js"],
+        },
+      ],
+      "no-console": "error",
+      "space-infix-ops": "error",
+      eqeqeq: ["error", "always"],
+      "prefer-template": "error",
+    },
   },
-  ignorePatterns: ["node_modules/**", "*.json", "coverage", "dist"],
-};
+  {
+    ...prettier,
+  },
+];
